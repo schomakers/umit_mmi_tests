1c943032103d216ad145f1d2c03513333afc9a3b
Andre Schomakers (Linux) <andre.scho24@googlemail.com>
2024-10-31 11:28:27 +0100

    SampleMocaTest erweitert um einige Testcases

--- tests/sampleMocaTest.js
+++ tests/sampleMocaTest.js
@@ -21,4 +21,20 @@ describe('#sum()', function () {
         expect(sum([1, 2, 3, 4, 5])).to.equal(15);
     })
 
+    it('should add negative numbers', function () {
+        expect(sum([-1, -2, -3, -4, -5])).to.equal(-15);
+    })
+
+    it('should return 0 for empty array', function () {
+        expect(sum([])).to.equal(0);
+    });
+    
+    it('should return single element for a one-item array', function () {
+        expect(sum([999])).to.equal(999);
+    });
+
+    it('should add floating-point numbers', function () {
+        expect(sum([1.5, 2.5, 3.5])).to.be.closeTo(7.5, 0.01);
+    });
+
 });
