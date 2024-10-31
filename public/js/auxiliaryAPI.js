00c464ba0b16adc18cfab918816b872d3ef29088
Andre Schomakers (Linux) <andre.scho24@googlemail.com>
2024-10-31 12:30:59 +0100

    Aufgabe 2 erledigt: Temperaturen und GreetingDependOnTime getestet

--- public/js/auxiliaryAPI.js
+++ public/js/auxiliaryAPI.js
@@ -1,31 +1,31 @@
-exports.celsiusToFahrenheit = function (x){
- return celsius*(9/5)+10
+
+// x is passed but celsius used as arg -> changed it to celsius to avoid ReferenceError
+
+exports.celsiusToFahrenheit = function (celsius){
+// celsius*(9/5)+32 is the correct formula
+    return celsius*(9/5)+32
 }
 
 exports.fahrenheitToCelcius = function (fahrenheit){
- if (true) {
-    return ((fahrenheit-32)/(5/9))
- }   
+// swapped (5/9) to (9/5 == 1.8) to get correct results
+    return ((fahrenheit-32)/(1.8))
 }
 
 exports.getGreetingDependOnTime =  function (myDate) {
+// What if "mydate" is no date object? Exception handling necessary!!: 
+    if (!(myDate instanceof Date)) {
+        throw new Error("Invalid input: expected a date object");
+    }
+
     let timeBegin = '06:00';
     let timeEnd = '22:00';
     const dateBegin = new Date('2020-01-01 ' + timeBegin);
     const dateEnd = new Date('2020-01-01 ' + timeEnd);
     const dateCurrent = new Date('2020-01-01 ' + myDate.getHours() + ":" + myDate.getMinutes());
 
-    if (dateBegin.getTime() < dateCurrent.getTime()) {
-        if (dateCurrent.getTime() >= dateEnd.getTime()) {
-
-            return "Guten Morgen"
-        }
-        else {
-            return "Guten Abend"
-        }
+    if (dateCurrent >= dateBegin && dateCurrent < dateEnd) {
+        return "Guten Morgen";
     } else {
-        newVariable = dateCurrent.getTime()
-        console.log("For debugging: ") + dateBegin.getTime() + " " + dateCurrent.getTime() + " " + dateEnd.getTime()
-        return "Guten Morgen"
+        return "Guten Abend";
     }
 }
--- tests/controllerTest.js
+++ tests/controllerTest.js
@@ -1,8 +1,72 @@
+// @author: Andre Schomakers
+
 const expect = require('chai').expect;
+const { describe } = require('mocha');
 const apiTemperature = require('../public/js/auxiliaryAPI')
 
-//TODO Unit Tests to implement
+
+// ---- 
+// fahrenheitToCelsius tested for 3 cases
 describe("fahrenheitToCelsius()", function(){
 
+	it("Input (F°) below 32", function() {
+	expect(Math.round(apiTemperature.fahrenheitToCelcius(30)*10)/10).to.equal(-1.1)
+	});
 
+	it("Input (F°) equals 0", function() {
+	expect(Math.round(apiTemperature.fahrenheitToCelcius(0)*10)/10).to.equal(-17.8)
+	});
+
+	it("Input (F°) above 32", function() {
+	expect(Math.round(apiTemperature.fahrenheitToCelcius(34)*10)/10).to.equal(1.1)
+	});
+});
+
+
+// ---- 
+// celsiusToFahrenheit
+describe("celsiusToFahrenheit()", function() {
+
+	it("Input (°C) below 0", function() {
+	expect(Math.round(apiTemperature.celsiusToFahrenheit(-2)*10)/10).to.equal(28.4)
+	});
+
+	it("Input (°C) is 0", function() {
+	expect(Math.round(apiTemperature.celsiusToFahrenheit(0)*10)/10).to.equal(32)
+	});
+
+	it("Input (°C) above 0", function() {
+	expect(Math.round(apiTemperature.celsiusToFahrenheit(2)*10)/10).to.equal(35.6)
+	});
+});
+
+
+// ---- 
+// cutoff times 06:00 and 22:00 for printing "GuMo" bzw. "Guten Abend"
+describe("getGreetingsDependendOnTime", function() {
+
+	it("Testing 6:00 == GuMo", function() {
+		expect(apiTemperature.getGreetingDependOnTime(new Date('2024-10-31 06:00:00'))).to.equal("Guten Morgen");
+	});
+	
+	it("Testing 22:00 == Guten Abend", function() {
+		expect(apiTemperature.getGreetingDependOnTime(new Date('2024-10-31 22:00:00'))).to.equal("Guten Abend");
+	});
+
+	// this is one is actually a lil redundant ..
+	it("Testing Edge Case 22:01", function() {
+		expect(apiTemperature.getGreetingDependOnTime(new Date('2024-10-31 22:01:00'))).to.equal("Guten Abend");
+	});
+
+	it("Testing Edge Case 05:59", function() {
+		expect(apiTemperature.getGreetingDependOnTime(new Date('2024-10-31 05:59:00'))).to.equal("Guten Abend");
+	});
+
+	// testing if an time is inputted as str object
+
+	it("Input is non-date-object", function() {
+		expect(() => apiTemperature.getGreetingDependOnTime("06:00")).to.throw(Error, "Invalid input: expected a date object");
+	});
+	
+	
 });
